2188f64ff4e618db8be4c676d621657fc95f602c
ahmed1700 <43181736+ahmed1700@users.noreply.github.com>
2020-03-04 14:59:12 +0200

    Add files via upload

---
+++ Contracts/IRepositroryManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface IRepositroryManager
+    {
+        IEmployeeRepository Employee { get; }
+        ICompanyRepository Company { get; }
+
+        Task SaveAsync();
+    }
+}
