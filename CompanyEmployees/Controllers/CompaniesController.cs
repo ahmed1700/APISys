e8e4ba27c098664453c8a7da8079fb574d727f68
ahmed1700 <43181736+ahmed1700@users.noreply.github.com>
2020-03-04 14:20:26 +0200

    Add files via upload

---
+++ CompanyEmployees/Controllers/CompaniesController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using System.Collections.Generic;
+using Entities.DataTransferObjects;
+using Entities.Models;
+using System.Linq;
+using CompanyEmployees.ModelBinders;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private ILoggerManager _logger;
+        private readonly IRepositroryManager _repositrory;
+        private readonly IMapper _mapper;
+        public CompaniesController(ILoggerManager logger, IRepositroryManager repositrory , IMapper mapper)
+        {
+            this._logger = logger;
+            this._repositrory = repositrory;
+            this._mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetCompanies()
+        {
+                var companies =await _repositrory.Company.GeAlltCompaniesAsync(trackChanges: false);
+                var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
+                return Ok(companiesDto);
+        }
+        [HttpGet("{id}" , Name ="CompanyByID")]
+        public IActionResult GetCompany(Guid id)
+        {
+            var company = _repositrory.Company.GetCompanyAsync(id, trackChanges: false);
+            if (company == null)
+            {
+                _logger.LogInfo($"Company Wiht Id : {id} doesn't exist in the database");
+                return NotFound();
+            }
+            else
+            {
+                var companyDto = _mapper.Map<CompanyDto>(company);
+                return Ok(companyDto);
+            }
+        }
+        [HttpGet("collection/({ids})" , Name ="CompanyCollection")]
+        public async Task<IActionResult> GetCompanyCollection(   [ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
+        {
+            if (ids ==null)
+            {
+                _logger.LogError("Parameter ids is null");
+                return BadRequest("Parameter ids is null");
+            }
+            var companyEntites =await _repositrory.Company.GetByIdsAsync(ids, trackChanges: false);
+            // Id valid + id not valid ==> Collection ? 
+            if (ids.Count() != companyEntites.Count())
+            {
+                _logger.LogError("Parameter ids is null");
+                return NotFound();
+            }
+            var companyToReturn = _mapper.Map<IEnumerable<CompanyDto>>(companyEntites);
+            return Ok(companyToReturn);
+        }
+        [HttpPost("collection")]
+        public async Task<IActionResult> CreateCompanyCollection([FromBody]IEnumerable<CompanyForCreateDto> companyCollection)
+        {
+            if (companyCollection == null)
+            {
+                _logger.LogError("Company Collection Object sent from client is null");
+                return BadRequest("Company Collection Object is null");
+            }
+            var companyEntites = _mapper.Map<IEnumerable<Company>>(companyCollection);
+            foreach (var company in companyEntites)
+            {
+                _repositrory.Company.CreateCompany(company);
+            }
+           await _repositrory.SaveAsync();
+            // return result ==> Saved 
+            var companyCollectiontoReturn = _mapper.Map<IEnumerable<CompanyDto>>(companyEntites);
+            var ids = String.Join(",", companyCollectiontoReturn.Select(c => c.Id));
+            return CreatedAtRoute("CompanyCollection", new { ids }, companyCollectiontoReturn);
+        }
+        [HttpPost]
+        public  IActionResult CreateCompany([FromBody]CompanyForCreateDto company)
+        {
+            if (company ==null)
+            {
+                _logger.LogError("CompanyForCreateDto Object sent from client is null");
+                return BadRequest("CompanyForCreateDto Object is null");
+            }
+            var companyEntity = _mapper.Map<Company>(company);
+            _repositrory.Company.CreateCompany(companyEntity);
+            _repositrory.SaveAsync();
+            // return result ==> Saved 
+            var companytoReturn= _mapper.Map<CompanyDto>(companyEntity);
+            return CreatedAtRoute("CompanyByID", new { id = companytoReturn.Id }, companytoReturn);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCompany(Guid id)
+        {
+            var company =await _repositrory.Company.GetCompanyAsync(id, trackChanges: false);
+            if (company == null)
+            {
+                _logger.LogInfo($"Company Wiht Id : {id} doesn't exist in the database");
+                return NotFound();
+            }
+            _repositrory.Company.DeleteCompany(company);
+            await _repositrory.SaveAsync();
+            return NoContent();
+        }
+        [HttpPut("{id}")]
+         public async Task<IActionResult> UpdateCompany(Guid id ,   [FromBody]CompanyForUpdateDto company)
+        {
+            if (company==null)
+            {
+                _logger.LogError("CompanyForUpdateDto object sent from client is  null");
+                return BadRequest("CompanyForUpdateDto object is  null");
+            }
+            var companyEntity = await _repositrory.Company.GetCompanyAsync(id, trackChanges: true);
+            if (companyEntity == null)
+            {
+                _logger.LogInfo($"Company Wiht Id : {id} doesn't exist in the database");
+                return NotFound();
+            }
+            _mapper.Map(company, companyEntity);
+            await _repositrory.SaveAsync();
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ CompanyEmployees/Controllers/EmployeeController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Contracts;
+using Entities.DataTransferObjects;
+using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Controllers
+{
+    [Route("api/companies/{companyId}/employees")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private ILoggerManager _logger;
+        private readonly IRepositroryManager _repositrory;
+        private readonly IMapper _mapper;
+        public EmployeeController(ILoggerManager logger, IRepositroryManager repositrory, IMapper mapper)
+        {
+            this._logger = logger;
+            this._repositrory = repositrory;
+            this._mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult GetEmployeesForCompany(Guid companyId)
+        {
+            var company = _repositrory.Company.GetCompanyAsync(companyId, trackChanges: false);
+            if (company == null)
+            {
+                _logger.LogInfo($"Company Wiht Id : {companyId} doesn't exist in the database");
+                return NotFound();
+            }
+            var employees = _repositrory.Employee.GetEmployees(companyId, trackChanges: false);
+            var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employees);
+            return Ok(employeesDto);
+        }
+        [HttpGet("{id}", Name = "GetEmployeeForCompany")]
+        public IActionResult GetEmployeeForCompany(Guid companyId, Guid id)
+        {
+            var company = _repositrory.Company.GetCompanyAsync(companyId, trackChanges: false);
+            if (company == null)
+            {
+                _logger.LogInfo($"Company Wiht Id : {companyId} doesn't exist in the database");
+                return NotFound();
+            }
+            var employee = _repositrory.Employee.GetEmployee(companyId, id, trackChanges: false);
+            var employeeDto = _mapper.Map<EmployeeDto>(employee);
+            return Ok(employeeDto);
+        }
+        [HttpPost]
+        public IActionResult CreateEmployeeForCompany(Guid companyId, [FromBody] EmployeeForCreateDto employee)
+        {
+            if (employee == null)
+            {
+                _logger.LogError("EmployeeForCreateDto Object sent from client is null");
+                return BadRequest("EmployeeForCreateDto Object is null");
+            }
+            if (!ModelState.IsValid)
+            {
+                _logger.LogError("Invalid Model state for the EmployeeForCreateDto object");
+                return UnprocessableEntity(ModelState);
+            }
+            // Sure ? if company id exists ?
+            var company = _repositrory.Company.GetCompanyAsync(companyId, trackChanges: false);
+            if (company == null)
+            {
+                _logger.LogInfo($"Company With d : {companyId} does't exist in the database.");
+                return NotFound();
+            }
+            // Everything is Okay emp/id 
+            var employeeEntity = _mapper.Map<Employee>(employee);
+
+            _repositrory.Employee.CreateEmployeeForCompany(companyId, employeeEntity);
+            _repositrory.SaveAsync();
+            var employeeToReturn = _mapper.Map<EmployeeDto>(employeeEntity);
+            // save --?url 
+            return CreatedAtRoute("GetEmployeeForCompany", new { companyId, id = employeeEntity.Id }, employeeToReturn);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteEmployeeForCompany(Guid companyId, Guid id)
+        {
+        var company = _repositrory.Company.GetCompanyAsync(companyId, trackChanges: false);
+        if (company == null)
+        {
+            _logger.LogInfo($"Company Wiht Id : {companyId} doesn't exist in the database");
+            return NotFound();
+        }
+            var employeeForComapny = _repositrory.Employee.GetEmployee(companyId, id, trackChanges: false);
+            if (employeeForComapny==null)
+            {
+                _logger.LogInfo($"Employee Wiht Id : {id} doesn't exist in the database");
+                return NotFound();
+            }
+            _repositrory.Employee.DeleteEmployee(employeeForComapny);
+            _repositrory.SaveAsync();
+            return NoContent(); // 204
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateEmployeeForCompany(Guid companyId, Guid id ,[FromBody] EmployeeForUpdateDto employee)
+        {
+            if (employee == null)
+            {
+                _logger.LogError("EmployeeForCreateDto Object sent from client is null");
+                return BadRequest("EmployeeForCreateDto Object is null");
+            }
+            var company = _repositrory.Company.GetCompanyAsync(companyId, trackChanges: false);
+            if (company == null)
+            {
+                _logger.LogInfo($"Company Wiht Id : {companyId} doesn't exist in the database");
+                return NotFound();
+            }
+            if (!ModelState.IsValid)
+            {
+                _logger.LogError("Invalid model state for the EmployeeForUpdateDto object");
+                return UnprocessableEntity(ModelState);
+            }
+            var employeeEntitiy = _repositrory.Employee.GetEmployee(companyId, id, trackChanges: true);
+            if (employeeEntitiy == null)
+            {
+                _logger.LogInfo($"Employee Wiht Id : {id} doesn't exist in the database");
+                return NotFound();
+            }
+            _mapper.Map(employee, employeeEntitiy);
+            _repositrory.SaveAsync();
+            return NoContent();
+        }
+        [HttpPatch("{id}")]
+        public IActionResult PariallyUpdateEmployeeForCompany(Guid companyId, Guid id,
+            [FromBody] JsonPatchDocument<EmployeeForUpdateDto> pathDoc)
+        {
+            if (pathDoc == null)
+            {
+                _logger.LogError("EmployeeForCreateDto Object sent from client is null");
+                return BadRequest("EmployeeForCreateDto Object is null");
+            }
+            var company = _repositrory.Company.GetCompanyAsync(companyId, trackChanges: false);
+            if (company == null)
+            {
+                _logger.LogInfo($"Company Wiht Id : {companyId} doesn't exist in the database");
+                return NotFound();
+            }
+            var employeeEntitiy = _repositrory.Employee.GetEmployee(companyId, id, trackChanges: true);
+            if (employeeEntitiy == null)
+            {
+                _logger.LogInfo($"Employee Wiht Id : {id} doesn't exist in the database");
+                return NotFound();
+            }
+            var employeeToPatch = _mapper.Map<EmployeeForUpdateDto>(employeeEntitiy);
+            pathDoc.ApplyTo(employeeToPatch , ModelState);
+            TryValidateModel(employeeToPatch);
+            if (!ModelState.IsValid)
+            {
+                _logger.LogError("Invalid model state for the Patch Document");
+                return UnprocessableEntity(ModelState);
+            }
+            _mapper.Map(employeeToPatch, employeeEntitiy);
+            _repositrory.SaveAsync();
+            return NoContent();
+        }
+    }
+}
